45d29c2f051efecc282ad727ec73208332959fe5
KwonEna <eeena214@gmail.com>
2021-10-25 20:44:58 +0900

    Study : PostCSS로 모듈화 이용하기

---
+++ PostCSS-test/src/app.jsx
@@ -0,0 +1,12 @@
+import './app.css';
+import Button1 from './components/button1';
+import Button2 from './components/button2';
+
+function App() {
+  return <>
+  <Button1/>
+  <Button2/>
+  </>
+}
+
+export default App;
