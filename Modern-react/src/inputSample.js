76e6fc9afbc2f5a5d89d39cb109041e24d70a631
KwonEna <eeena214@gmail.com>
2021-11-13 23:38:35 +0900

    Study : 여러 input의 상태 관리

--- Modern-react/src/inputSample.js
+++ Modern-react/src/inputSample.js
@@ -1,23 +1,43 @@
 import React, { useState } from "react";
 
 function InputSample() {
-  const [text, setText] = useState("");
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = inputs;
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { name, value } = e.target; // = e.target.name, e.target.value
+
+    setInputs({
+      // 객체 상태를 업데이트 할 땐, 기존 상태를 복사해줘야 함(불변성)
+      ...inputs,
+      [name]: value,
+    });
   };
 
   const onReset = () => {
-    setText("");
+    setInputs({
+      name: "",
+      nickname: "",
+    });
   };
 
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
       <button onClick={onReset}>초기화</button>
       <div>
         <b>값 : </b>
-        {text}
+        {name}({nickname})
       </div>
     </div>
   );
